111a6f7b6984cc9ba6c489d5b7c03dc82536edbc
nicolasgabrieloficial <61585068+nicolasgabrieloficial@users.noreply.github.com>
2020-07-18 01:20:53 -0300

    Add files via upload

--- Aula27_28_29_30/Produto.cs
+++ Aula27_28_29_30/Produto.cs
@@ -1,66 +1,140 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Aula27_28_29_30
-
 {
     public class Produto
     {
         public int Codigo { get; set; }
         public string Nome { get; set; }
         public float Preco { get; set; }
-        private const string PATH = "Database/Produto.csv";
+        private const string PATH = "Database/produto.csv";
+
         public Produto()
-        {
-            //Criando arquivo se não existir
-            if( !File.Exists(PATH))
+        {   
+            string pasta = PATH.Split('/')[0];
+            if(!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+            // Cria o arquivo caso não exista
+            if(!File.Exists(PATH))
             {
                 File.Create(PATH).Close();
             }
         }
-        public void Inserir(Produto p)
-        {
-            var linha = new string[] {p.PrepararLinhaCSV(p) };
-            File.AppendAllLines(PATH,linha);
+
+        public void Cadastrar(Produto prod)
+        {            
+            string[] linha = new string[] { PrepararLinha(prod) };
+            File.AppendAllLines(PATH, linha);
         }
-        private string PrepararLinhaCSV(Produto p)
-        {
-            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco};";
-        }
+
         public List<Produto> Ler()
         {
-            //criamos uma lista de produtos
-            List<Produto> produtos = new List<Produto>();
-            
+            // Criamos uma lista para guardar o retorno
+            List<Produto> prod = new List<Produto>();
 
-            //Transformaos as linhas em um array de strings
+            // Lemos o .csv e separamos em um array de linhas
             string[] linhas = File.ReadAllLines(PATH);
-            
 
-            //Varremos este array de strings
-            foreach(var linha in linhas)
+            // Varremos nossas linhas
+            foreach(string linha in linhas)
             {
-                //quebramos cada linha em partes, pegando
-                string[] dados = linha.Split(";");
-    
+                
+                string[] dado = linha.Split(";");
 
-                //Tratamos os dados e adicionamos em um novo produto
-                Produto prod = new Produto();
-                prod.Codigo  = Int32.Parse( Separar(dados[0]) );
-                prod.Nome    = Separar(dados[1]);
-                prod.Preco   = float.Parse( Separar(dados[2]) );
+               
 
-                //adicionamos o item na lista antes de retorná-la
-                produtos.Add(prod);
+                Produto p   = new Produto();
+                p.Codigo    = Int32.Parse( Separar(dado[0]) );
+                p.Nome      = Separar(dado[1]);
+                p.Preco     = float.Parse( Separar(dado[2]) );
+
+                prod.Add(p);
             }
 
-            return produtos;    
+            prod = prod.OrderBy(z => z.Nome).ToList();
+
+            return prod;
         }
+
+        public List<Produto> Filtrar(string _nome){
+            return Ler().FindAll(x => x.Nome == _nome);
+        }
+
+        public void Remover(string _termo)
+        {
+            // Criamos uma lista de linhas para fazer uma espécie de backup 
+           
+            List<string> linhas = new List<string>();
+
+            using(StreamReader arquivo = new StreamReader(PATH))
+            {
+                string linha;
+                while((linha = arquivo.ReadLine()) != null){
+                    linhas.Add(linha);
+                }
+            }
+
+            linhas.RemoveAll(a => a.Contains(_termo));
+
+            ReescreverCSV(linhas);
+        }
+
+        public void Alterar(Produto produtoAlterado){
+
+            // Criamos uma lista de linhas para fazer uma espécie de backup 
+            List<string> linhas = new List<string>();
+
+            using(StreamReader arquivo = new StreamReader(PATH))
+            {
+                string linha;
+                while((linha = arquivo.ReadLine()) != null){
+                    linhas.Add(linha);
+                }
+            }
+
+            linhas.RemoveAll(z => z.Split(";")[0].Split("=")[1] == produtoAlterado.Codigo.ToString());
+
+            linhas.Add( PrepararLinha( produtoAlterado ) );
+
+            ReescreverCSV(linhas);
+        }
+
+        /// <summary>
+        /// Reescreve o CSV
+        /// </summary>
+        /// <param name="lines">Lista de linhas</param>
+        private void ReescreverCSV(List<string> lines){
+            // Criamos uma forma de reescrever o arquivo sem as linhas removidas
+            using(StreamWriter output = new StreamWriter(PATH))
+            {
+                foreach(string ln in lines)
+                {
+                    output.Write(ln+"\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Método que separa o símbolo de = da string do csv
+        /// </summary>
+        /// <param name="dado">Coluna do csv separada</param>
+        /// <returns>string somente com o valor da coluna</returns>
         public string Separar(string dado)
         {
-            
             return dado.Split("=")[1];
         }
+
+        private string PrepararLinha(Produto p)
+        {
+            return $"codigo={p.Codigo};nome={p.Nome};preco={p.Preco}";
+        }
+
+
     }
 }
--- Aula27_28_29_30/Program.cs
+++ Aula27_28_29_30/Program.cs
@@ -7,21 +7,29 @@ namespace Aula27_28_29_30
     {
         static void Main(string[] args)
         {
-           Produto p = new Produto();
-           p.Codigo = 10;
-           p.Nome = "Messi";
-           p.Preco =10000f;
+            Produto p1 = new Produto();
+            p1.Codigo = 9;
+            p1.Nome = "Lewandowski";
+            p1.Preco = 9000f;
 
-           p.Inserir(p);
+            p1.Cadastrar(p1);
 
-           List<Produto> lista = new List<Produto>();
-           lista = p.Ler();
+            Produto alterado = new Produto();
+            alterado.Codigo = 11;
+            alterado.Nome = "Neymar";
+            alterado.Preco = 11000f;
 
-           foreach (Produto item in lista)
-           {
-               Console.WriteLine($"R${item.Preco} - {item.Nome}");
-           }
-           lista.Find( p => p.Nome == "Messi" );
+            p1.Alterar(alterado);
+
+            p1.Remover("Lewandowski");
+
+            List<Produto> lista = p1.Ler();
+
+            foreach (Produto item in lista)
+            {
+                Console.WriteLine($"R$ {item.Preco} - {item.Nome}");
+            }
+
         }
     }
 }
